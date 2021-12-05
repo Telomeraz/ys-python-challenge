32e8236643ec65b1c6a8b322f700d2e7ceee3c98
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-12-05 15:59:51 +0300

    Change publish data context

--- ys_python_challenge/utils/decorators.py
+++ ys_python_challenge/utils/decorators.py
@@ -5,7 +5,7 @@ def pub_data(func):
     def wrapper(obj, *args, **kwargs):
         response = func(obj, *args, **kwargs)
 
-        publish_data(response.channel_name, {"message": response.id})
+        publish_data(response.channel_name, response.to_dict())
 
         return response
 
