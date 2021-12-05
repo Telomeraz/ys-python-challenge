432a2bfa14aabd7def2c134b2a55380ef9c2cc08
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-12-05 21:12:16 +0300

    Add tests for orders views

---
+++ ys_python_challenge/orders/tests/test_views.py
@@ -0,0 +1,35 @@
+from rest_framework import status
+
+from django.urls import reverse
+
+from utils.base_test import BaseTest
+
+
+class OrderTest(BaseTest):
+    def test_order_create_view(self):
+        # import ipdb;ipdb.set_trace()
+        data = {
+            "address": "2377 Twin Willow Lane Jacksonville NC North Carolina",
+            "restaurant": 1,
+            "items": [{"unit_price": 5, "quantity": 1}],
+        }
+        response = self.client.post(
+            reverse("order-create"),
+            data,
+            **self.auth_headers,
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_order_complete_view(self):
+        response = self.client.post(
+            reverse("order-complete", kwargs={"order_id": 1}),
+            **self.auth_headers,
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_order_list_view(self):
+        response = self.client.get(
+            reverse("order-list"),
+            **self.auth_headers,
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
