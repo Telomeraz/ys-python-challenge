a75311713e72c9d7966b69e1d60eedea8af03302
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-12-05 14:05:20 +0300

    Create data publisher decorator

---
+++ ys_python_challenge/utils/decorators.py
@@ -0,0 +1,12 @@
+from pub_sub.pub import publish_data
+
+
+def pub_data(func):
+    def wrapper(obj, *args, **kwargs):
+        response = func(obj, *args, **kwargs)
+
+        publish_data(response.channel_name, {"message": response.id})
+
+        return response
+
+    return wrapper
